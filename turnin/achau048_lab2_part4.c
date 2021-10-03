e85ddec83bbed488bb91e6cb82eb2259e30f71ad
ChauAbdul786 <ChauAbdul786@gmail.com>
2021-10-02 19:37:28 -0700

    Completed Part 4

---
+++ turnin/achau048_lab2_part4.c
@@ -0,0 +1,60 @@
+/*	Author: Abdullah Chaudhry
+ *  Partner(s) Name: 
+ *	Lab Section: 21
+ *	Assignment: Lab #2  Exercise #4
+ *	Exercise Description: Amusement Park Ride Weights - Ports A - C represent 8-bit weight sensors(in kg) for seats on a ride. If the total weight of the vehicle exceeds 140kg than PD0 should be set to 1. If the weight difference between seats A and C is greater than 80kg PD1 should be set to 1. 
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+	/* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF; //Configure's Port A's 8 pins as inputs
+	DDRB = 0x00; PORTB = 0xFF; //Configure's Port B's 8 pins as inputs
+	DDRC = 0x00; PORTC = 0xFF; //Configure's Port C's 8 pins as inputs
+	DDRD = 0xFF; PORTD = 0x00; //Configure's Port D's 8 pins as outputs
+				   //Initializes PORTD(output) to 0x00;	
+
+	/* Insert your solution below */
+	unsigned char tmpA = 0x00; //To hold PINA
+	unsigned char tmpB = 0x00; //To hold PINB
+	unsigned char tmpC = 0x00; //To hold PINC
+	unsigned char tmpD = 0x00; //To hold PORTD
+
+	unsigned short totalWeight = 0; //Unsigned short to hold total weight
+	signed short weightDifference = 0; //For weight difference between seats A and C
+						//Short is needed because signed char range is 
+						// -128 - 127
+	while (1) {
+		//Input 
+		tmpA = PINA;
+		tmpB = PINB;
+		tmpC = PINC;
+
+		totalWeight = tmpA + tmpB + tmpC;
+		if(totalWeight > 140){
+			tmpD = (tmpD | 0x01); //DDDD DDD1
+		}else{
+			tmpD = (tmpD & 0xFE); //DDDD DDD0
+		}
+		
+		weightDifference = tmpA - tmpC;
+		if(weightDifference > 80 || weightDifference < -80){ //Need to check <-80 if seat
+									//C is heavier than seat A
+			tmpD = (tmpD | 0x02); //DDDD DD1D
+		}else{
+			tmpD = (tmpD & 0xFD); //DDDD DD0D
+		}
+		
+
+		//Output
+		PORTD = tmpD;
+	}
+	
+    
+	return 0;
+}
---
+++ turnin/achau048_lab2_part4_tests.py
@@ -0,0 +1,66 @@
+# Array of tests to run (in order)
+# Each test contains
+#   description - 
+#   steps - A list of steps to perform, each step can have
+#       inputs - A list of tuples for the inputs to apply at that step
+#       *time - The time (in ms) to wait before continuing to the next step 
+#           and before checking expected values for this step. The time should be a multiple of
+#           the period of the system
+#       *iterations - The number of clock ticks to wait (periods)
+#       expected - The expected value at the end of this step (after the "time" has elapsed.) 
+#           If this value is incorrect the test will fail early before completing.
+#       * only one of these should be used
+#   expected - The expected output (as a list of tuples) at the end of this test
+# An example set of tests is shown below. It is important to note that these tests are not "unit tests" in 
+# that they are not ran in isolation but in the order shown and the state of the device is not reset or 
+# altered in between executions (unless preconditions are used).
+tests = [ {'description': 'PINA: 0x00, PINB: 0x00, PINC: 0x00 => PORTD: 0x00 (No Weight)',
+	'steps': [ {'inputs': [('PINA',0x00),('PINB',0x00),('PINC',0x00)], 'iterations': 5 } ],
+	'expected': [('PORTD',0x00)],
+	},
+	{'description': 'PINA: 0x21, PINB: 0x1F, PINC: 0x00 => PORTD: 0x00 (Empty SeatC)',
+	'steps': [ {'inputs': [('PINA',0x21),('PINB',0x1F),('PINC',0x00)], 'iterations': 5 } ],
+	'expected': [('PORTD',0x00)],
+	},
+	{'description': 'PINA: 0x1E, PINB: 0x00, PINC: 0x0A => PORTD: 0x00 (Empty SeatB)',
+	'steps': [ {'inputs': [('PINA',0x1E),('PINB',0x00),('PINC',0x0A)], 'iterations': 5 } ],
+	'expected': [('PORTD',0x00)],
+	},
+	{'description': 'PINA: 0x00, PINB: 0x09, PINC: 0x20 => PORTD: 0x00 (Empty SeatA)',
+	'steps': [ {'inputs': [('PINA',0x00),('PINB',0x09),('PINC',0x20)], 'iterations': 5 } ],
+	'expected': [('PORTD',0x00)],
+	},
+	{'description': 'PINA: 0x1E, PINB: 0x10, PINC: 0x21 => PORTD: 0x02 (All Three Seats Occupied)',
+	'steps': [ {'inputs': [('PINA',0x1E),('PINB',0x10),('PINC',0x21)], 'iterations': 5 } ],
+	'expected': [('PORTD',0x00)],
+	},
+	{'description': 'PINA: 0x32, PINB: 0x28, PINC: 0x32 => PORTD: 0x00 (Weight Adds Up To 140kg)',
+	'steps': [ {'inputs': [('PINA',0x32),('PINB',0x28),('PINC',0x32)], 'iterations': 5 } ],
+	'expected': [('PORTD',0x00)],
+	},
+	{'description': 'PINA: 0xFF, PINB: 0xFF, PINC: 0xFF => PORTD: 0x02 (All Seats Full Weight)',
+	'steps': [ {'inputs': [('PINA',0xFF),('PINB',0xFF),('PINC',0xFF)], 'iterations': 5 } ],
+	'expected': [('PORTD',0x01)],
+	},
+	{'description': 'PINA: 0x00, PINB: 0xFF, PINC: 0x00 => PORTD: 0x00 (Only SeatB Full Weight)',
+	'steps': [ {'inputs': [('PINA',0x00),('PINB',0xFF),('PINC',0x00)], 'iterations': 5 } ],
+	'expected': [('PORTD',0x01)],
+	},
+	{'description': 'PINA: 0x51, PINB: 0x00, PINC: 0x00 => PORTD: 0x02 (Unbalanced Heavy SeatA)',
+	'steps': [ {'inputs': [('PINA',0x51),('PINB',0x00),('PINC',0x00)], 'iterations': 5 } ],
+	'expected': [('PORTD',0x02)],
+	},
+	{'description': 'PINA: 0x00, PINB: 0xFF, PINC: 0x51 => PORTD: 0x00 (Unbalanced Heavy SeatC)',
+	'steps': [ {'inputs': [('PINA',0x00),('PINB',0x00),('PINC',0x51)], 'iterations': 5 } ],
+	'expected': [('PORTD',0x02)],
+	},
+	{'description': 'PINA: 0xFF, PINB: 0x00, PINC: 0x00 => PORTD: 0x00 (Only SeatA Full Weight)',
+	'steps': [ {'inputs': [('PINA',0xFF),('PINB',0x00),('PINC',0x00)], 'iterations': 5 } ],
+	'expected': [('PORTD',0x03)],
+	},
+	{'description': 'PINA: 0x00, PINB: 0x00, PINC: 0xFF => PORTD: 0x00 (Only SeatC Full Weight)',
+	'steps': [ {'inputs': [('PINA',0x00),('PINB',0x00),('PINC',0xFF)], 'iterations': 5 } ],
+	'expected': [('PORTD',0x03)],
+	},
+	]
+#watch = ['PORTC']
